33ff728833ad301bded746a125db56c91d5a0109
lechihoang <lechihoang73@gmail.com>
2025-05-16 21:46:19 +0700

    add course material

---
+++ The_Complete_Full-Stack_Web_Development Bootcamp/Dicee+Challenge+-+Starting+Files/index.js
@@ -0,0 +1,18 @@
+document.querySelector('.img1').setAttribute('src', './images/dice6.png');
+document.querySelector('.img2').setAttribute('src', './images/dice6.png');
+
+var randomNumber1 = Math.floor(Math.random() * 6) + 1;
+var randomNumber2 = Math.floor(Math.random() * 6) + 1;
+
+document.querySelector('.img1').setAttribute('src', `./images/dice${randomNumber1}.png`);
+document.querySelector('.img2').setAttribute('src', `./images/dice${randomNumber2}.png`);
+
+if (randomNumber1 > randomNumber2) {
+    document.querySelector('h1').innerHTML = "🚩 Player 1 Win!";
+}
+else if (randomNumber1 == randomNumber2) {
+    document.querySelector('h1').innerHTML = "Draw!"
+}
+else {
+    document.querySelector('h1').innerHTML = "Player 2 Win! 🚩";
+}
